b18b049bab85a306825b77f0ddda66e0501c600c
David Huang <david.huang@check24.de>
2022-12-01 11:01:26 +0100

    adding solution for day 1

---
+++ day1/main.py
@@ -0,0 +1,24 @@
+if __name__ == '__main__':
+    elfCount = 1
+    maxElf = 0
+    maxCalorieSum = -1
+    currentCalorieSum = 0
+
+    with open('input.txt', 'r') as calorieList:
+        for line in calorieList:
+            line = line.strip()
+
+            if line == '':
+                if maxElf == 0 or currentCalorieSum > maxCalorieSum:
+                    maxElf = elfCount
+                    maxCalorieSum = currentCalorieSum
+                elfCount += 1
+                currentElfSum = 0
+                continue
+
+            currentCalorieSum += int(line)
+
+    if maxElf == 0 or currentCalorieSum > maxCalorieSum:
+        maxElf = elfCount
+
+    print(maxCalorieSum)
