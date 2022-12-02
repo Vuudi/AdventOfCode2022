2bf383206ac7dd3e071741f0ec6ffeeca08f3a86
David Huang <david.huang@check24.de>
2022-12-02 09:58:26 +0100

    adding solution for day 2 part 2

--- day2/main.py
+++ day2/main.py
@@ -2,17 +2,29 @@ if __name__ == '__main__':
     totalScore = 0
     with open('input.txt', 'r') as strategyGuide:
         for line in strategyGuide:
+            # part 1
+            #
+            # opponent = ord(line[0]) - 64
+            # you = ord(line[2]) - 87
+            #
+            # outcome = (you - opponent) % 3
+
+            # if outcome == 2:
+            #     outcome = 0
+            # elif outcome == 1:
+            #     outcome = 6
+            # else:
+            #     outcome = 3
+
             opponent = ord(line[0]) - 64
-            you = ord(line[2]) - 87
+            outcome = (ord(line[2]) - 88) * 3
 
-            outcome = (you - opponent) % 3
-
-            if outcome == 2:
-                outcome = 0
-            elif outcome == 1:
-                outcome = 6
+            if outcome == 0:
+                you = opponent
+            elif outcome == 3:
+                you = (opponent + 2) % 3
             else:
-                outcome = 3
+                you = (opponent + 1) % 3
 
             totalScore += you + outcome
 
