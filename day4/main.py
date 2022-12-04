de4be98242002ff8525db8dce44802e65dc4950f
David Huang <david.huang@check24.de>
2022-12-05 00:37:32 +0100

    adding solution for day 4 part 1

--- day4/main.py
+++ day4/main.py
@@ -7,6 +7,12 @@ if __name__ == '__main__':
             sectionsOne = pairs[0].split('-')
             sectionsTwo = pairs[1].split('-')
 
+            # part 1
+            #
+            # if ((int(sectionsOne[0]) <= int(sectionsTwo[0]) and int(sectionsOne[1]) >= int(sectionsTwo[1])) or
+            #         (int(sectionsOne[0]) >= int(sectionsTwo[0]) and int(sectionsOne[1]) <= int(sectionsTwo[1]))):
+            #     totalContainingPairs += 1
+
             start = int(sectionsOne[0]) if int(sectionsOne[0]) > int(sectionsTwo[0]) else int(sectionsTwo[0])
             end = int(sectionsOne[1]) if int(sectionsOne[1]) < int(sectionsTwo[1]) else int(sectionsTwo[1])
 
