ea419a8c47035bc6153e6072d240cc0f3ace6d8d
David Huang <david.huang@check24.de>
2022-12-05 00:35:20 +0100

    adding solution for day 4

---
+++ day4/main.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    totalOverlappingPairs = 0
+    with open('input.txt', 'r') as assignments:
+        for assignment in assignments:
+            assignment = assignment.strip()
+            pairs = assignment.split(',')
+            sectionsOne = pairs[0].split('-')
+            sectionsTwo = pairs[1].split('-')
+
+            start = int(sectionsOne[0]) if int(sectionsOne[0]) > int(sectionsTwo[0]) else int(sectionsTwo[0])
+            end = int(sectionsOne[1]) if int(sectionsOne[1]) < int(sectionsTwo[1]) else int(sectionsTwo[1])
+
+            totalOverlappingPairs += 1 if end - start >= 0 else 0
+
+    print(totalOverlappingPairs)
